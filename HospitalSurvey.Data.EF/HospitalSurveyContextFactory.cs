8b6df89c7090e8253e77f3469b21707e27b7f934
Hoang Than <d.p.anh57@gmail.com>
2020-05-13 22:38:22 +0700

    update factory

--- HospitalSurvey.Data.EF/HospitalSurveyContextFactory.cs
+++ HospitalSurvey.Data.EF/HospitalSurveyContextFactory.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace HospitalSurvey.Data.EF
 {
-    class HospitalSurveyContextFactory
+
+
+    public class HospitalSurveyContextFactory : IDesignTimeDbContextFactory<HospitalSurveyDbContext>
     {
+        public HospitalSurveyDbContext CreateDbContext(string[] args)
+        {
+            /* Read current folder and get connection string in appsettings.json file */
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json")
+                .Build();
+
+
+            var connectionString = configuration.GetConnectionString("mamEduDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<HospitalSurveyDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new HospitalSurveyDbContext(optionsBuilder.Options);
+        }
     }
 }
